d79e60c27289df913af3e9f412526a4436eb83e6
Mahmad HasanBaba <mahmadhasanbaba9@gmail.com>
2021-08-15 18:45:24 +0530

    this is jenkins project

---
+++ src/main/java/com/baba/controller/WelcomeController.java
@@ -0,0 +1,9 @@
+package com.baba.controller;
+
+public class WelcomeController {
+
+	public static void main(String[] args) {
+      System.out.println("WELCOME TO JENKINS PROJECT WITH MAVEN...!!!");
+	}
+
+}
